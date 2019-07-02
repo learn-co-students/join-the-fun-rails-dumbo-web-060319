89f5e8beb809f9c7bbf1869642fdc03007abbbcb
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-01 21:31:19 -0400

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,17 @@
 class CreateRides < ActiveRecord::Migration
   def change
+
+  	create_table :taxis do |t|
+  	  t.string :name
+  	end
+
+  	create_table :passengers do |t|
+  	  t.string :name
+  	end
+
     create_table :rides do |t|
+      t.belongs_to :taxi, index: true
+      t.belongs_to :passenger, index: true
       t.timestamps null: false
     end
   end
